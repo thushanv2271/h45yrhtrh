5c3bc5fd2c364e0fd460a55d49bf0ce230c0b35b
thushanv2271 <thushan.v@azendtech.com>
2025-10-15 13:21:34 +0530

    Add files via upload

---
+++ EfaConfigs/EfaConfigurationIntegrationTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using Domain.EfaConfigs;
+using FluentAssertions;
+using IntegrationTests.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace IntegrationTests.EfaConfigs;
+
+public class EfaConfigurationIntegrationTests 
+{
+
+    [Fact]
+    public void TestRunner_ShouldWork()
+    {
+        // Arrange
+        int a = 2;
+        int b = 3;
+
+        // Act
+        int sum = a + b;
+
+        // Assert
+        sum.Should().Be(5);
+    }
+}
